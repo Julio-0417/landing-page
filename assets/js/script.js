94b4a90955c229ee705980f661d49dee10a7fdcb
unknown <julio.s.silva0417@gmail.com>
2021-06-22 20:43:32 -0300

    Landing Page, versão 2.0

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+/*
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementByName()
+por Classe: getElementByClassName()
+por Seletor: querySelector() mais comum
+*/
+
+let email = document.querySelector('#exampleFormControlInput1')
+let assunto = document.querySelector('#exampleFormControlTextarea1')
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+
+    } else {
+        txtEmail.innerHTML = 'E-mail Válido'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if(assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar() {
+    if (emailOk == true && assuntoOk == true) {
+        alert ('Formulário enviado com sucesso!')
+    } else {
+        alert ('Preencha o formulário corretamente antes de enviar!')
+    }
+}
+
+function mapaZoom() {
+    mapa.style.width = '800px'
+    mapa.style.height = '600px' 
+}
+
+function mapaNormal() {
+    mapa.style.width = '400px'
+    mapa.style.height = '300px'
+
+}
+
